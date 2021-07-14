27e51c298b2a721464fdad0477041d64ef620bba
JimmyMiels <jimmymiels@hotmail.com>
2021-07-15 00:16:43 +0200

    Registratie module via email and password met rol.
    Gebruiker bewaren in FireStore.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 import {AngularFireModule} from '@angular/fire';
 import {AngularFireAuthModule} from '@angular/fire/auth';
 import {AngularFirestoreModule} from '@angular/fire/firestore';
@@ -15,10 +16,12 @@ import { environment } from 'src/environments/environment';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+  imports: [BrowserModule,
+  IonicModule.forRoot(),
+  AppRoutingModule,
   AngularFireModule.initializeApp(environment.firebaseConfig),
   AngularFireAuthModule,
-  AngularFirestoreModule
+  AngularFirestoreModule,
 ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
